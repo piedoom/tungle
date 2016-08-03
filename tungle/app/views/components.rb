8daa7d9975865372baad9a68f323bcd6a0b12f26
aa <a>
2016-08-03 14:18:47 -0700

    added reactrb

---
+++ tungle/app/views/components.rb
@@ -0,0 +1,15 @@
+# app/views/components.rb
+require 'opal'
+require 'react'
+require 'reactrb'
+if React::IsomorphicHelpers.on_opal_client?
+  require 'opal-jquery'
+  require 'browser'
+  require 'browser/interval'
+  require 'browser/delay'
+  # add any additional requires that can ONLY run on client here
+end
+
+
+
+require_tree './components'
